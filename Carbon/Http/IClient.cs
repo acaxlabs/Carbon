a322ca7dbd7dcee9254eb32204c20a35f14ad202
Josh Hickerson <hickerson_j@vimo.com>
2018-03-22 19:13:15 -0500

    added http namespace and net namespace added iclient and client

---
+++ Carbon/Http/IClient.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Carbon.Http
+{
+    public interface IClient
+    {
+        string BaseUrl { get; set; }
+
+        /// <summary>
+        /// Makes an http "Get" request for a resource
+        /// </summary>
+        /// <typeparam name="T">the type the response will be deserialized to from json</typeparam>
+        /// <param name="url">the resource's endpoint</param>
+        /// <returns>the resource of type "T"</returns>
+        T Get<T>(string url);
+        /// <summary>
+        /// Makes an http "Get" request for a resource
+        /// </summary>
+        /// <param name="url">the resource's endpoint</param>
+        /// <returns>the resource as json</returns>
+        string Get(string url);
+        /// <summary>
+        /// Makes a http "POST" request for a resource
+        /// </summary>
+        /// <typeparam name="T">the type the response will be deserialized to from json</typeparam>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to post</param>
+        /// <returns>the resource of type "T"</returns>
+        T Post<T>(string url, object data);
+        /// <summary>
+        /// Makes a http "POST" request for a resource
+        /// </summary>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to post</param>
+        /// <returns>the resource as json</returns>
+        string Post(string url, object data);
+        /// <summary>
+        /// Makes a http "Put" request for a resource
+        /// </summary>
+        /// <typeparam name="T">the type the response will be deserialized to from json</typeparam>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to Put</param>
+        /// <returns>the resource of type "T"</returns>
+        T Put<T>(string url, object data);
+        /// <summary>
+        /// Makes a http "Put" request for a resource
+        /// </summary>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to Put</param>
+        /// <returns>the resource as json</returns>
+        string Put(string url, object data);
+        /// <summary>
+        /// Makes a http "Remove" request for a resource
+        /// </summary>
+        /// <typeparam name="T">the type the response will be deserialized to from json</typeparam>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to Remove</param>
+        /// <returns>the resource of type "T"</returns>
+        T Remove<T>(string url, object data);
+        /// <summary>
+        /// Makes a http "Remove" request for a resource
+        /// </summary>
+        /// <param name="url">the resource's endpoint</param>
+        /// <param name="data">the data to Remove</param>
+        /// <returns>the resource as json</returns>
+        string Remove(string url, object data);
+    }
+}
---
+++ CarbonTests/Net/ClientTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Carbon.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carbon.Net.Tests
+{
+    [TestClass()]
+    public class ClientTests
+    {
+        //Use https://www.mocky to build request to test
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            try
+            {
+                var res = new Client().Get("http://www.mocky.io/v2/5ab444412f00006400ca3b31");
+                Assert.IsNotNull(res);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void PostTest()
+        {
+            try
+            {
+                var res = new Client().Post("http://www.mocky.io/v2/5ab444412f00006400ca3b31", new { message = "test" });
+                Assert.IsNotNull(res);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void PutTest()
+        {
+            try
+            {
+                var res = new Client().Put("http://www.mocky.io/v2/5ab444412f00006400ca3b31", new { message = "test"});
+                Assert.IsNotNull(res);
+            }
+            catch (Exception ex)
+            {
+
+                Assert.Fail(ex.Message);
+            }
+        }
+
+        
+    }
+}
