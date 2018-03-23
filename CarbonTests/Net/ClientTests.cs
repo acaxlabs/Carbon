e83f853cd3dff1b8ebacf6bf14405e89bb67006c
Cody Arnold <djdivinity7@gmail.com>
2018-03-23 16:01:42 -0500

    Revert indention to spaces

--- CarbonTests/Net/ClientTests.cs
+++ CarbonTests/Net/ClientTests.cs
@@ -3,52 +3,52 @@ using Carbon.Net;
 
 namespace CarbonTests.Net
 {
-	[TestClass]
+    [TestClass]
     public class ClientTests
     {
         [TestMethod]
-		[TestCategory("Smoke")]
-		[TestCategory("Get")]
+        [TestCategory("Smoke")]
+        [TestCategory("Get")]
         public void GetSendsValidGetRequest()
         {
-			var res = new Client().Get("http://httpbin.org/get");
-			Assert.IsNotNull(res);
+            var res = new Client().Get("http://httpbin.org/get");
+            Assert.IsNotNull(res);
         }
 
         [TestMethod]
-		[TestCategory("Smoke")]
-		[TestCategory("Post")]
+        [TestCategory("Smoke")]
+        [TestCategory("Post")]
         public void PostSendsValidPostRequest()
         {
-			var res = new Client().Post("http://httpbin.org/post", new { message = "test" });
-			Assert.IsNotNull(res);
-		}
+            var res = new Client().Post("http://httpbin.org/post", new { message = "test" });
+            Assert.IsNotNull(res);
+        }
 
-		[TestMethod]
-		[TestCategory("Smoke")]
-		[TestCategory("Put")]
+        [TestMethod]
+        [TestCategory("Smoke")]
+        [TestCategory("Put")]
         public void PutSendsValidPutRequest()
         {
-			var res = new Client().Put("http://httpbin.org/put", new { message = "test" });
-			Assert.IsNotNull(res);
-		}
+            var res = new Client().Put("http://httpbin.org/put", new { message = "test" });
+            Assert.IsNotNull(res);
+        }
 
-		[TestMethod]
-		[TestCategory("Post")]
-		public void PostSendsDataInRequest()
-		{
-			string postTestString = "ThisIsThePostTestStringImLookingFor";
-			var res = new Client().Post("http://httpbin.org/post", new { message = postTestString });
-			Assert.IsTrue(res.Contains(postTestString), "Expected string was not found in response");
-		}
+        [TestMethod]
+        [TestCategory("Post")]
+        public void PostSendsDataInRequest()
+        {
+            string postTestString = "ThisIsThePostTestStringImLookingFor";
+            var res = new Client().Post("http://httpbin.org/post", new { message = postTestString });
+            Assert.IsTrue(res.Contains(postTestString), "Expected string was not found in response");
+        }
 
-		[TestMethod]
-		[TestCategory("Put")]
-		public void PutSendsDataInRequest()
-		{
-			string putTestString = "ThisIsThePutTestStringImLookingFor";
-			var res = new Client().Put("http://httpbin.org/put", new { message = putTestString });
-			Assert.IsTrue(res.Contains(putTestString), "Expected string was not found in response");
-		}
-	}
+        [TestMethod]
+        [TestCategory("Put")]
+        public void PutSendsDataInRequest()
+        {
+            string putTestString = "ThisIsThePutTestStringImLookingFor";
+            var res = new Client().Put("http://httpbin.org/put", new { message = putTestString });
+            Assert.IsTrue(res.Contains(putTestString), "Expected string was not found in response");
+        }
+    }
 }
