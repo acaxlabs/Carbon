c8eda43ae9a5b02527a0e85fd350cc3387cc1eaf
Cody Arnold <djdivinity7@gmail.com>
2018-03-23 13:54:56 -0500

    Add Put and Post data tests

--- CarbonTests/Net/ClientTests.cs
+++ CarbonTests/Net/ClientTests.cs
@@ -6,10 +6,9 @@ namespace CarbonTests.Net
 	[TestClass]
     public class ClientTests
     {
-        //Use https://www.mocky to build request to test
-
         [TestMethod]
 		[TestCategory("Smoke")]
+		[TestCategory("Get")]
         public void GetSendsValidGetRequest()
         {
 			var res = new Client().Get("http://httpbin.org/get");
@@ -18,6 +17,7 @@ namespace CarbonTests.Net
 
         [TestMethod]
 		[TestCategory("Smoke")]
+		[TestCategory("Post")]
         public void PostSendsValidPostRequest()
         {
 			var res = new Client().Post("http://httpbin.org/post", new { message = "test" });
@@ -26,10 +26,29 @@ namespace CarbonTests.Net
 
 		[TestMethod]
 		[TestCategory("Smoke")]
+		[TestCategory("Put")]
         public void PutSendsValidPutRequest()
         {
 			var res = new Client().Put("http://httpbin.org/put", new { message = "test" });
 			Assert.IsNotNull(res);
+		}
+
+		[TestMethod]
+		[TestCategory("Post")]
+		public void PostSendsDataInRequest()
+		{
+			string postTestString = "ThisIsThePostTestStringImLookingFor";
+			var res = new Client().Post("http://httpbin.org/post", new { message = postTestString });
+			Assert.IsTrue(res.Contains(postTestString), "Expected string was not found in response");
+		}
+
+		[TestMethod]
+		[TestCategory("Put")]
+		public void PutSendsDataInRequest()
+		{
+			string putTestString = "ThisIsThePutTestStringImLookingFor";
+			var res = new Client().Put("http://httpbin.org/put", new { message = putTestString });
+			Assert.IsTrue(res.Contains(putTestString), "Expected string was not found in response");
 		}
 	}
 }
